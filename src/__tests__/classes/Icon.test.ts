ee2bf56ab9762e326284ed766044bce4f3d3903d
Mullp <mullp@protonmail.com>
2022-03-16 21:00:24 +0100

    Update Icon.test.ts

--- src/__tests__/classes/Icon.test.ts
+++ src/__tests__/classes/Icon.test.ts
@@ -6,3 +6,7 @@ const client = new Client();
 test("Icon test", async () => {
   expect(await client.icon.get("GOLDEN_APPLE", true)).toBeInstanceOf(Icon);
 });
+
+test("All icons test", async () => {
+  expect(await client.icon.getAll()).toBeInstanceOf(Array);
+});
